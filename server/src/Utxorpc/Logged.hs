47540f0f6fa88cd8a7df0a31296b89426a68b002
Dominic Mayhew <dominic.j.mayhew@gmail.com>
2024-02-08 13:02:21 -0800

    Add haddock comments to server logging types

--- server/src/Utxorpc/Logged.hs
+++ server/src/Utxorpc/Logged.hs
@@ -22,6 +22,12 @@ import Network.GRPC.Server (ServiceHandler, UnaryHandler)
 import Network.GRPC.Server.Handlers.Trans (ServerStream (..), ServerStreamHandler, serverStream, unary)
 import Network.Wai (Request (..))
 
+-- | A collection of logging functions that runs in the same monad as
+-- the request handlers. Monadic state is passed along throughout the
+-- lifecycle of responding to a request. This means that changes to the
+-- monadic state in the request logger is seen by the stream logger, stream
+-- handler and logger, and reply logger. An `unlift` function to run the monad
+-- in IO is provided to @'runUtxorpc'@.
 data UtxorpcServerLogger m = UtxorpcServerLogger
   { requestLogger :: RequestLogger m,
     replyLogger :: ReplyLogger m,
@@ -29,36 +35,57 @@ data UtxorpcServerLogger m = UtxorpcServerLogger
     serverStreamEndLogger :: ServerStreamEndLogger m
   }
 
+-- | Log requests
 type RequestLogger m =
   forall i.
   (Show i) =>
+  -- | The RPC path
   BS.ByteString ->
+  -- | Request metadata
   Request ->
+  -- | A UUID generated for this request and passed to stream and reply loggers.
   UUID ->
+  -- | The request message
   i ->
   m ()
 
+-- | Log replies
 type ReplyLogger m =
   forall o.
   (Show o) =>
+  -- | The RPC path
   BS.ByteString ->
+  -- | Request metadata
   Request ->
+  -- Generated for the request that caused this reply
   UUID ->
+  -- | The reply message
   o ->
   m ()
 
+-- | Log server stream messages
 type ServerStreamLogger m =
   forall o.
   (Show o) =>
+  -- | The RPC path
   BS.ByteString ->
+  -- | Request metadata
   Request ->
+  -- | The UUID generated for the request that generated this stream,
+  -- and the 0-based index of the message in the stream.
   (UUID, Int) ->
+  -- | The stream message
   o ->
   m ()
 
+-- | Log the end of a server stream
 type ServerStreamEndLogger m =
+  -- | The RPC path
   BS.ByteString ->
+  -- | Request metadata
   Request ->
+  -- | The UUID generated for the request that generated this stream,
+  -- and the 0-based index of the message in the stream.
   (UUID, Int) ->
   m ()
 
